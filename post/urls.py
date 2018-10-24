9ff2204d37ca8fd9173c2756b7478f1b03bf64d7
Onkar Tibe <onkartibe@users.noreply.github.com>
2018-10-24 17:41:12 +0530

    Update urls.py

--- post/urls.py
+++ post/urls.py
@@ -3,5 +3,5 @@ from post import views
 
 urlpatterns = [
     url('posts/', views.post_list),
-    url(r'post/(?P<slug>[\w-]+)/$', views.post_details),
+    url(r'posts/(?P<slug>[\w-]+)/$', views.post_details),
 ]
