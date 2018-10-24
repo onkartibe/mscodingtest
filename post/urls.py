82f115ab8210b82242a3157c45a0af5e7e810a39
Onkar Tibe <onkartibe@users.noreply.github.com>
2018-10-24 17:51:17 +0530

    Update urls.py

--- post/urls.py
+++ post/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from post import views
 
 urlpatterns = [
-    url('posts/', views.post_list),
+    url('posts', views.post_list),
     url(r'posts?slug=(?P<slug>[\w-]+)/$', views.post_details),
 ]
