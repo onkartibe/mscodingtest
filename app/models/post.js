5d39305497aa485fee45ba60856f55cbf3dc82cd
onkartibe <onkar.tibe@gmail.com>
2018-10-25 13:06:54 +0530

    Author added and Tags provision done

--- app/models/post.js
+++ app/models/post.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     title: DS.attr('string'),
-    auther: DS.attr('string'),
+    author: DS.attr('string'),
     slug: DS.attr('string'),
     description: DS.attr('string'),
     tags: DS.attr('string'),
